852ed68836f30cabd8cdc035217424b128b8b4f5
Tim Davies <tim@practicalparticipation.co.uk>
2022-06-28 12:43:58 +0200

    First commit

--- README.md
+++ README.md
@@ -1,1 +1,37 @@
-# LandPortal-Demo
+# Notre Dame Global Adaptation Initiative -> LandPortal Data Model
+
+This project prepares data from the Notre Dame Global Adaptation Initiative to be imported into LandPortal: the one-stop platform for statistical data on land governance. 
+
+You can:
+
+* Explore this dataset on the Land Portal 
+
+* Query this dataset in via our SPARQL endpoint
+
+* Explore the data pipeline that prepares this data (below)
+
+You can also:
+
+* Find the full list of data sources and indicators imported into the Land Portal here. 
+
+* Suggest, contribute or sponsor new data sources
+
+
+## Dataset overview
+
+| Property | Value |
+|----------|-------|
+| Publisher | University of Notre Dame |
+| License | Copyright © 2022 University of Notre Dame |
+| Frequency | ? |
+
+
+## Indicator mapping
+
+
+## Workflow
+
+Last updated: DATA
+
+Validation reports: HERE
+
---
+++ etl.py
@@ -0,0 +1,41 @@
+from dataflows import Flow, load, dump_to_path, dump_to_zip, printer, add_metadata
+from dataflows import sort_rows, filter_rows, find_replace, delete_fields, set_type, validate, unpivot
+import zipfile
+import requests
+from io import BytesIO
+
+def etl():
+    # Download the latest data and extract
+    file = requests.get("https://gain.nd.edu/assets/437409/resources.zip")
+    zipped = zipfile.ZipFile(BytesIO(file.content))
+    zipped.extractall('input/')
+
+    unpivoting_fields = [
+    { 'name': '([0-9]{4})', 'keys': {'year': r'\1'} }]
+
+    # A newly created column header would be 'year' with type 'year':
+    extra_keys = [ {'name': 'year', 'type': 'year'} ]
+    # And values will be placed in the 'value' column with type 'string':
+    extra_value = {'name': 'value', 'type': 'string'}
+
+    # Convert to a common format
+    flow = Flow(
+        # Load inputs
+        load('input/resources/gain/gain.csv', format='csv', ),
+        # Process them (if necessary)
+
+        unpivot(unpivoting_fields, extra_keys, extra_value),
+        
+        # Save the results
+        add_metadata(name='GAIN Index', title='Gain Index'),
+
+        # Output
+        dump_to_path(out_path='output'),
+        printer()
+
+    )
+    flow.process()
+
+
+if __name__ == '__main__':
+    etl()
